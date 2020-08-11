fbf4498f77db5b992e8308024b0af8d7162260ef
KaiBarrett <morderkai@hotmail.com>
2020-08-11 15:19:05 +0100

    adding the pgadmin post setup

---
+++ runtime/TrayIcon.h
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// pgAdmin 4 - PostgreSQL Tools
+//
+// Copyright (C) 2013 - 2020, The pgAdmin Development Team
+// This software is released under the PostgreSQL Licence
+//
+// TrayIcon.h - Manages the tray icon
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef TRAYICON_H
+#define TRAYICON_H
+
+#include "pgAdmin4.h"
+
+// QT headers
+#include <QWidget>
+#include "MenuActions.h"
+
+class TrayIcon : public QWidget
+{
+    Q_OBJECT
+
+public:
+    TrayIcon();
+
+    void Init();
+    void enableShutdownMenu();
+    void setMenuActions(MenuActions * menuActions);
+
+private:
+    void createTrayIcon();
+    void createActions();
+
+    QAction *m_newAction = Q_NULLPTR;
+    QAction *m_copyUrlAction = Q_NULLPTR;
+    QAction *m_configAction = Q_NULLPTR;
+    QAction *m_logAction = Q_NULLPTR;
+    QAction *m_quitAction = Q_NULLPTR;
+
+    QSystemTrayIcon *m_trayIcon = Q_NULLPTR;
+    QMenu *m_trayIconMenu = Q_NULLPTR;
+
+    MenuActions *m_menuActions = Q_NULLPTR;
+};
+
+#endif // TRAYICON_H
